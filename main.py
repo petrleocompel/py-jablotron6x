7d0f2837740a59dfc0d19f9abf773174a2be36a0
Petr Leo Compel <petrleocompel@gmail.com>
2019-05-30 20:57:51 +0200

    code styling

--- main.py
+++ main.py
@@ -9,25 +9,16 @@ from argparse import ArgumentParser
 from ja2mqtt import Jablotron2mqtt
 
 
-
 if __name__ == "__main__":
 
-    parser = ArgumentParser(description=\
-        "Runs mqtt bridge for jablotron alarms")
-    parser.add_argument('--serial-port', '-p', required=True, 
-        help="Serial port where the JA-80T is connected.")
-    parser.add_argument('--host', '-H', default="localhost",
-        help="Mqtt host to connect to (default: localhost)")
-    parser.add_argument('--mqtt-port', '-P', default="1883",
-        help="Mqtt port to connect to (default: 1883)")
-    parser.add_argument('--topic', '-t', default="alarm",
-        help="Mqtt topic where to publish messages (default: alarm)") 
-    parser.add_argument('-v', dest='loglevel', action='store_const',
-        const=logging.INFO, help="verbose output.")
-    parser.add_argument('-d', dest='loglevel', action='store_const',
-        const=logging.DEBUG, help="debug output.")
-    parser.add_argument('-q', dest='loglevel', action='store_const',
-        const=logging.ERROR, help="quiet mode.")
+    parser = ArgumentParser(description="Runs mqtt bridge for jablotron alarms")
+    parser.add_argument('--serial-port', '-p', required=True,  help="Serial port where the JA-80T is connected.")
+    parser.add_argument('--host', '-H', default="localhost", help="Mqtt host to connect to (default: localhost)")
+    parser.add_argument('--mqtt-port', '-P', default="1883", help="Mqtt port to connect to (default: 1883)")
+    parser.add_argument('--topic', '-t', default="alarm", help="Mqtt topic where to publish messages (default: alarm)")
+    parser.add_argument('-v', dest='loglevel', action='store_const', const=logging.INFO, help="verbose output.")
+    parser.add_argument('-d', dest='loglevel', action='store_const', const=logging.DEBUG, help="debug output.")
+    parser.add_argument('-q', dest='loglevel', action='store_const', const=logging.ERROR, help="quiet mode.")
 
     args = parser.parse_args()
 
@@ -40,5 +31,5 @@ if __name__ == "__main__":
                             mqtt_topic=args.topic) as j2m:
             j2m.loop_forever()
     except socket.error, e:
-        print("Failed to connect to mqtt host: " +args.host + " - " + str(e), file=sys.stderr)
+        print("Failed to connect to mqtt host: " + args.host + " - " + str(e), file=sys.stderr)
 
